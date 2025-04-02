de6798739e129fb54c6645af51fe814b5cd09ea5
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-04-02 13:37:43 -0500

    changed name of starter app to student.java. store student ID,store student name,store grades from student

---
+++ Student.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Student {
+    // store student ID
+    private String studentID;
+    // store student name
+    private String name;
+    // store grades from student
+    private ArrayList<Integer> grades;
+
+
+}
